e093873d16bdbc686622ab126bcc8cbf5be85b2a
user-Roma <buster2020@yandex.ru>
2021-12-15 02:59:45 +0300

    15.12.21 sprint 7: postreview fixes/3

--- homework.py
+++ homework.py
@@ -52,136 +52,64 @@ def send_message(bot, message):
     """Sends a message to Telegram chat."""
     if message is None:
         return
-    try:
-        bot.send_message(
-            chat_id=TELEGRAM_CHAT_ID,
-            text=message
-        )
-        if 'Сбой' in message:
-            logger.info('FAILURE message sending')
-            return
-        logger.info('STATUS message sending')
-    except Exception as error:
-        logger.exception(error)
-        raise
+    # Тут сделал логирование, исключение обработается потом в цикле, так?
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text=message
+    )
+    if 'Сбой' in message:
+        logger.info('FAILURE message sending')
+        return
+    logger.info('STATUS message sending')
 
 
 def get_api_answer(current_timestamp) -> dict:
     """Makes a request to API -> converts API answer to python."""
+    # Наконец-то я понял! Спасибо!
     timestamp = current_timestamp
     params = {'from_date': timestamp}
-    # По заданию я должен логировать это - я создал исключения для различных
-    # ситуаций и далее при срабатывании исключения - оно автоматически будет
-    # логироваться для меня - именно то, которое сработало + тк я прописал в
-    # классах исключений суть случившейся проблемы - это описание будет
-    # передано в лог как раз при вызове данного исключения -
-    # и мне не нужно прописывать под каждым событием его логировние
-    try:
-        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-        if response.status_code == HTTPStatus.NOT_FOUND:
-            raise ApiNotFoundError
-        if response.status_code != HTTPStatus.OK:
-            raise ApiConnectionFailed(response.status_code)
-        return response.json()
-    except (Exception, json.decoder.JSONDecodeError) as error:
-        logger.exception(error)
-        raise
+    response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    if response.status_code == HTTPStatus.NOT_FOUND:
+        raise ApiNotFoundError
+    if response.status_code != HTTPStatus.OK:
+        raise ApiConnectionFailed(response.status_code)
+    return response.json()
 
 
 def check_response(response) -> list:
     """Checking API answer."""
-    # Тут у меня такая же мысль, как описал выше - ко мне приходит ответ:
-    # в блоке try  - я по логике событий сначала проверяю в правильном ли
-    # формате пришел запрос,
-    # далее если формат подошел - есть ли там нужные ключ,
-    # далее если ключа нет делаю возврат пустого списка (так просят в задании),
-    # далее я извлекаю сам ключ и проверяю подходит ли нам формат его значения
-    # и если все в порядке я возвращаю результат.
-    # При этом, как только в любом из вышеперечисленных пунктов сработает
-    # заготовленное исключение - оно сразу будет передано в обработку
-    # и будет создан его лог (на основании описания в классе исключения),
-    # поэтому я обернул все эти события в try -
-    # потому что при любом из них будет обработано исключение + залогировано
-    try:
-        if not isinstance(response, dict):
-            raise ResponseNotDictError(response)
-        if 'homeworks' not in response:
-            raise KeyNotExistsError('homeworks')
-        # В задании указано, что функция должна возвращать пустой список,
-        # тут мы его и возвращаем, а потом уже проверяем его статус в функции
-        # проверки статусов. Хотя эту проверку сразу можно сделать и в данной
-        # функции, но она отвечает за проверку структуры ответа, поэтому,
-        # тут я проверку не делаю, а просто возвращаю пустой список
-        # и не вызываю исключений.
-        if not response['homeworks']:
-            return []
-        homework = response.get('homeworks')
-        if not isinstance(homework, list):
-            raise HomeworksNotListError(homework)
-        return homework[0]
-    except Exception as error:
-        logger.exception(error)
-        raise
+    if not isinstance(response, dict):
+        raise ResponseNotDictError(response)
+    if 'homeworks' not in response:
+        raise KeyNotExistsError('homeworks')
+    if not response['homeworks']:
+        return []
+    homework = response.get('homeworks')
+    if not isinstance(homework, list):
+        raise HomeworksNotListError(homework)
+    return homework[0]
 
 
 def parse_status(homework) -> str:
     """Generate message for sending."""
-    # Тут я руководствовался такой же логикой как описал выше,
-    # если так делать плохая практака тогда мне следует
-    # обрабатывать + логировать в таком виде как написано ниже
-    # или я что-то упускаю:
-
-    # if not isinstance(response, dict):
-    #     logger.exception(ResponseNotDictError(response))
-    #     raise ResponseNotDictError(response)
-    # if 'homeworks' not in response:
-    #     logger.exception(KeyNotExistsError('homeworks'))
-    #     raise KeyNotExistsError('homeworks')
-    # if not response['homeworks']:
-    #     return
-    # homework = response.get('homeworks')
-    # if not isinstance(homework, list):
-    #     logger.exception(HomeworksNotListError(homework))
-    #     raise HomeworksNotListError(homework)
-    # return homework[0]
-
-    # Возможно я что-то не так понимаю и нам не нужно все логировать,
-    # что обрабатывается исключениями и наоборот,
-    # но у нас ведь все обрабатываемые исключения относятся к классу ERROR -
-    # поэтому мы должны лоигровать их все, исходя из нашего ДЗ
-    # и если не выводить все эти условия, которые могут могут привести
-    # к некорректной работе модуля в блок try -
-    # тогда нужно будет прописавать под каждым условий
-    # его обработку и логирование,
-    # что повлечет большее кол-во повторяющегося кода
-    # или как еше можно это реализовать?
-
     if not homework:
         message = 'No new statuses from Master'
         logger.debug(message)
         return
-    try:
-        if 'homework_name' not in homework:
-            raise KeyNotExistsError('homework_name')
-        if 'status' not in homework:
-            raise KeyNotExistsError('status')
-        homework_name = homework.get('homework_name')
-        homework_status = homework.get('status')
-        if homework_status not in HOMEWORK_STATUSES:
-            raise HomeworkStatusError(homework_status)
-    except Exception as error:
-        logger.exception(error)
-        raise
+    if 'homework_name' not in homework:
+        raise KeyNotExistsError('homework_name')
+    if 'status' not in homework:
+        raise KeyNotExistsError('status')
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if homework_status not in HOMEWORK_STATUSES:
+        raise HomeworkStatusError(homework_status)
     verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens() -> bool:
     """Checks the availability of env variables."""
-    # Если есть возможность - просьба присылать ссылку на источник,
-    # который поясняет, почему строчка кода ниже работает в данной функции,
-    # тк при проверке тестами - функция возвращает мне значение последней
-    # переменной, а не True/False
     return PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID
 
 
@@ -194,7 +122,7 @@ def main() -> None:
                 raise CheckTokensError(var)
 
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_timestamp = "int(time.time())"
     error_filter = None
 
     while True:
@@ -205,7 +133,8 @@ def main() -> None:
             message = parse_status(homework)
             send_message(bot, message)
             time.sleep(RETRY_TIME)
-        except Exception as error:
+        except (Exception, json.decoder.JSONDecodeError) as error:
+            logger.exception(error)
             message = f'Сбой в работе программы:{error} {format_exc()}'
             if message != error_filter:
                 error_filter = message
